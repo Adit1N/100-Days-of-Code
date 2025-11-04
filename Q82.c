a7c258f4a4062e3b8ba49bc3795506d04404fb10
Adit1N <aditnautiyal10@gmail.com>
2025-11-04 13:37:55 +0530

    Printing each character of a string on a new line.

---
+++ Q82.c
@@ -0,0 +1,23 @@
+//Print each character of a string on a new line.
+
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i = 0;
+
+    // Input the string
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Print each character on a new line
+    printf("Characters in the string:\n");
+    while (str[i] != '\0') {
+        // Exclude the newline character added by fgets
+        if (str[i] != '\n') {
+            printf("%c\n", str[i]);
+        }
+        i++;
+    }
+
+    return 0; 
+}
